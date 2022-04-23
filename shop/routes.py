f8efc6d8907f831a03c4cde9661d1fe49d727adc
Magomedov888 <m.abdulla05@gmail.com>
2022-04-23 19:48:43 +0300

    site working

--- shop/routes.py
+++ shop/routes.py
@@ -11,6 +11,42 @@ def index():
 def blog():
     return render_template('blog.html')
 
+@app.route('/single_blog')
+def single_blog():
+    return render_template('single_blog.html')
+
 @app.route('/cart')
 def cart():
     return render_template('cart.html')
+
+@app.route('/single_product')
+def single_product():
+    return render_template('single_product.html')
+
+@app.route('/category')
+def category():
+    return render_template('category.html')
+
+@app.route('/checkout')
+def checkout():
+    return render_template('checkout.html')
+
+@app.route('/tracking_order')
+def tracking_order():
+    return render_template('tracking_order.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/confirmation')
+def confirmation():
+    return render_template('confirmation.html')
