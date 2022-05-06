60dc080bf65427412498506b85adbd26531b3bc8
Magomedov888 <m.abdulla05@gmail.com>
2022-05-06 15:11:10 +0300

    Добавил db user

--- shop/models.py
+++ shop/models.py
@@ -12,3 +12,13 @@ class Product(db.Model):
 
     def __repr__(self) -> str:
         return self.title
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer(), primary_key=True)
+    email = db.Column(db.String(), nullable=False, unique=True)
+    password = db.Column(db.String(), nullable=False, unique=True)
+    isAdmin = db.Column(db.Boolean, default=False)
+
+    def __repr__(self) -> str:
+        return self.email
